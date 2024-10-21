06dd61b6a662d520de86c5d8f5d5e8fa68a20d51
Ayush Verma <132675966+Ayushxvx@users.noreply.github.com>
2024-10-21 15:42:38 +0530

    Update Projects.jsx

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -22,6 +22,20 @@ export default function Projects(){
 <div data-aos="fade-up" class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 p-2 m-2 mx-auto">
         <img class="rounded-t-lg" src="./project1.png" alt="Mystic Forum Project" />
     <div class="p-5">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Youtube Clone</h5>
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">It is a full stack youtube clone with authentication and many more features including like , dislike and comments.</p>
+        <a target="_blank" href="https://vayush.pythonanywhere.com/" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            Read more
+             <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+            </svg>
+        </a>
+    </div>
+</div>
+            
+<div data-aos="fade-up" class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 p-2 m-2 mx-auto">
+        <img class="rounded-t-lg" src="./project1.png" alt="Mystic Forum Project" />
+    <div class="p-5">
             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Mystic Forum</h5>
         <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">It is a full stack web application with authentication and many more features including like , dislike and comments.</p>
         <a target="_blank" href="https://ayushv2003.pythonanywhere.com/" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
